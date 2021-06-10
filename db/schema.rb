10b90d81369dba0b514decc5bb734d24bdaa4aac
ytmk0116 <ytmk0116@gmail.com>
2021-06-10 21:34:08 +0000

    migration追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_10_183313) do
+ActiveRecord::Schema.define(version: 2021_06_10_204516) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
